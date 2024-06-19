12764c13a8458db6b831728ec265a8243e8c955d
paul449 <paul_bilbatua@hotmail.com>
2024-06-19 16:53:35 -0500

    seed

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -2,17 +2,11 @@ import {gql} from '@apollo/client';
 //export query GET_ME
 export const GET_ME = gql`
     query Me{
-        _id
-        username
-        email
-        saveBook{
-            BookId{
-                authors
-                description
-                title
-                image
-                link
-            }
+        me {
+            _id
+            username
+            email
+            saveBook
         }
     }
 `
