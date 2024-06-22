4eceb7949afd7f64aefbd5f5b27ce31f9483a3cf
paul449 <paul_bilbatua@hotmail.com>
2024-06-22 14:34:12 -0500

    server

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -2,3 +2,5 @@
 const {typeDefs} = require('./typeDefs');
 //importing resolvers from schema folder
 const {resolvers} = require('./resolvers');
+//exporting typedefs and resolvers
+module.exports = {typeDefs, resolvers};
